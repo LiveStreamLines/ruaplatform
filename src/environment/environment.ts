ac8dd70ae14a420a71e6d1e251c6d206900179ff
Ammar Omer <amar@livestreamlines.com>
2025-11-17 17:53:45 +0400

    enviroment

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  backend: 'http://localhost:5000',
+  backend: 'http://10.100.72.10/backend',
   proxy: 'https://lsl-platform.com/proxy',
   hik: 'http://localhost:3000',
 };
--- src/environment/environments.ts
+++ src/environment/environments.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  backend: 'http://localhost:5000',
-  proxy: 'https://ahcwatch.awjholding.com/proxy/',
+  backend: 'http://10.100.72.10/backend',
+  proxy: 'http://10.100.72.10/proxy/',
   hik: 'https://ahcwatch.awjholding.com/backend',
 };
