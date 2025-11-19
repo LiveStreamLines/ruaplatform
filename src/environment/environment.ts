59e13c7f364294cb9fc486a54fa2ec6405daed4c
Ammar Omer <amar@livestreamlines.com>
2025-11-19 20:57:19 +0400

    Update environment configuration for production and development settings
    - Changed backend and proxy URLs to point to the new server.
    - Updated 'hik' URLs for both production and development environments.
    - Modified MSAL redirect and postLogoutRedirect URIs to reflect the new production URL.

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,14 +1,14 @@
 export const environment = {
   production: false,
   backend: 'http://10.100.72.10/backend',
-  proxy: 'https://lsl-platform.com/proxy',
-  hik: 'http://localhost:3000',
+  proxy: 'http://10.100.72.10/proxy/',
+  hik: 'https://10.100.72.10/backend',
   msalConfig: {
     auth: {
       clientId: 'YOUR_AZURE_AD_CLIENT_ID', // Replace with your Client ID
       authority: 'https://login.microsoftonline.com/YOUR_TENANT_ID', // Replace with your Tenant ID
-      redirectUri: 'http://localhost:4200', // Update for production
-      postLogoutRedirectUri: 'http://localhost:4200' // Update for production
+      redirectUri: 'https://time-lapse.ruaalmadinah.com', // Update with your production URL
+      postLogoutRedirectUri: 'https://time-lapse.ruaalmadinah.com' // Update with your production URL
     },
     cache: {
       cacheLocation: 'sessionStorage',
--- src/environment/environments.ts
+++ src/environment/environments.ts
@@ -2,13 +2,13 @@ export const environment = {
   production: true,
   backend: 'http://10.100.72.10/backend',
   proxy: 'http://10.100.72.10/proxy/',
-  hik: 'https://ahcwatch.awjholding.com/backend',
+  hik: 'https://10.100.72.10/backend',
   msalConfig: {
     auth: {
       clientId: 'YOUR_AZURE_AD_CLIENT_ID', // Replace with your Client ID
       authority: 'https://login.microsoftonline.com/YOUR_TENANT_ID', // Replace with your Tenant ID
-      redirectUri: 'https://ahcwatch.awjholding.com', // Update with your production URL
-      postLogoutRedirectUri: 'https://ahcwatch.awjholding.com' // Update with your production URL
+      redirectUri: 'https://time-lapse.ruaalmadinah.com', // Update with your production URL
+      postLogoutRedirectUri: 'https://time-lapse.ruaalmadinah.com' // Update with your production URL
     },
     cache: {
       cacheLocation: 'sessionStorage',
