d1030390439d6c5033b181054077998b854c5641
Ammar Omer <amar@livestreamlines.com>
2025-11-22 21:47:36 +0400

    Update MSAL configuration in environment file with production client and tenant IDs

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -5,8 +5,8 @@ export const environment = {
   hik: 'https://10.100.72.10/backend',
   msalConfig: {
     auth: {
-      clientId: 'YOUR_AZURE_AD_CLIENT_ID', // Replace with your Client ID
-      authority: 'https://login.microsoftonline.com/YOUR_TENANT_ID', // Replace with your Tenant ID
+      clientId: '42d12237-e179-4b53-9856-fc6eb19ebb3d', // Replace with your Client ID
+      authority: 'https://login.microsoftonline.com/1a450556-a74f-4135-ac56-506d6b3bea05', // Replace with your Tenant ID
       redirectUri: 'https://time-lapse.ruaalmadinah.com', // Update with your production URL
       postLogoutRedirectUri: 'https://time-lapse.ruaalmadinah.com' // Update with your production URL
     },
