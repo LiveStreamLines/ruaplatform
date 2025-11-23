95616104d43ab9e09f312da985cb93d041c8b62e
Ammar Omer <amar@livestreamlines.com>
2025-11-23 13:07:40 +0400

    check

--- src/environment/environments.ts
+++ src/environment/environments.ts
@@ -7,7 +7,7 @@ export const environment = {
     auth: {
       clientId: '42d12237-e179-4b53-9856-fc6eb19ebb3d', // Replace with your Client ID
       authority: 'https://login.microsoftonline.com/1a450556-a74f-4135-ac56-506d6b3bea05', // Replace with your Tenant ID
-      redirectUri: 'https://time-lapse.ruaalmadinah.com/login', // Update with your production URL
+      redirectUri: 'https://time-lapse.ruaalmadinah.com', // Update with your production URL
       postLogoutRedirectUri: 'https://time-lapse.ruaalmadinah.com' // Update with your production URL
     },
     cache: {
